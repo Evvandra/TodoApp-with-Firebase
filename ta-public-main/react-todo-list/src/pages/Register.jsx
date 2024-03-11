41cba55508f4cccbdee96b0c5f71786fce505988
Vandra <evandraharya777@gmail.com>
2024-03-12 02:26:44 +0700

    Update Register.jsx

--- ta-public-main/react-todo-list/src/pages/Register.jsx
+++ ta-public-main/react-todo-list/src/pages/Register.jsx
@@ -26,10 +26,10 @@ export default function Register() {
         await addDoc(collection(db, 'users'), {
           uid: userCredential.user.uid, 
           email: userCredentials.email,
-          Name: userCredentials.userName
+          name: userCredentials.userName
         });
   
-        navigate("/Landing");
+        navigate("/");
       })
       .catch((error) => {
         setError(error.message);
@@ -92,7 +92,7 @@ export default function Register() {
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
-                    <Link to="/Landing">
+                    <Link to="/">
                       <div className="mt-6">
                           <button onClick={(e)=>{handleSignup(e)}} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                               Register
